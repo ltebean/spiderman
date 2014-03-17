fd7afc5633f77ad3c6656a5714b417774f9e13e3
ltebean <yucong1118@gmail.com>
2014-03-17 09:00:44 +0800

    add intro

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-## spiderman
+Spiderman works much like alfred workflow and ios storyboard, you write crawler by defining some components, using segues to connet them, finally write some script to specify data that needs to be transfer from one component to the next;
+
+The whole logic is stored in this form:
+
+	startup:
+	  url: http://www.dianping.com/search/category/1/0
+	  component: shopList
+	components:
+	  shopList:   
+	    type: pageProcessor
+	    segues:
+	      - to: shopList
+	        func: 
+	          |
+	          var nextPage=$(".NextPage");
+	          if(!nextPage){
+	          	  return;
+	          }         
+	          offer("http://www.dianping.com"+nextPage.attr("href"));
+	      - to: shop
+	        func: 
+	          |
+	          $(".BL").each(function() {
+	          	  offer("http://www.dianping.com"+($(this).attr("href")));
+	          })
+	  shop:   
+	    type: pageProcessor
+	    segues:
+	      - to: mongodb
+	        func: 
+	          |
+	          offer({
+	          	  shopName:$(".shop-title").text()
+	          });
+	  mongodb:   
+	    type: mongodbAdaptor
+	    host: 127.0.0.1
+	    port: 27017
+	    collection: shop
--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var yaml=require('js-yaml');
-var fs=require('fs');
+var yaml = require('js-yaml');
+var fs = require('fs');
 var util = require('util');
 
 
-var componentFactory={
-	'pageProcessor':require('./lib/pageProcessor'),
-	'mongodbAdaptor':require('./lib/mongodbAdaptor')
+var componentFactory = {
+	'pageProcessor': require('./lib/pageProcessor'),
+	'mongodbAdaptor': require('./lib/mongodbAdaptor')
 }
 
-var config=yaml.load(fs.readFileSync('./config.yaml').toString());
+var config = yaml.load(fs.readFileSync('./config.yaml').toString());
 
 //console.log(util.inspect(config, {showHidden: false, depth: null}));//console.dir(config)
 
-var components={}
+var components = {}
 
 for (var componentName in config.components) {
-	var componentConfig=config.components[componentName];
-	components[componentName]=new componentFactory[componentConfig.type](componentName,componentConfig,processSegue)
+	var componentConfig = config.components[componentName];
+	components[componentName] = new componentFactory[componentConfig.type](componentName, componentConfig, processSegue)
 }
 
-function processSegue(to,data){
+function processSegue(to, data) {
 	//console.log(to+":"+data)
 	components[to].process(data);
 }
-	
+
 components[config.startup.component].process(config.startup.url);
