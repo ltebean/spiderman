8876b42aaeacdcae0dd83642e3eb448643d4539b
ltebean <yucong1118@gmail.com>
2014-03-17 12:56:13 +0800

    refactor

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
 var yaml = require('js-yaml');
 var fs = require('fs');
-var util = require('util');
-
-
-var componentFactory = {
-	'pageProcessor': require('./lib/pageProcessor'),
-	'mongodbAdaptor': require('./lib/mongodbAdaptor')
-}
+var Spiderman = require('./lib/spiderman.js');
 
 var config = yaml.load(fs.readFileSync('./config.yaml').toString());
 
-//console.log(util.inspect(config, {showHidden: false, depth: null}));//console.dir(config)
-
-var components = {}
-
-for (var componentName in config.components) {
-	var componentConfig = config.components[componentName];
-	components[componentName] = new componentFactory[componentConfig.type](componentName, componentConfig, processSegue)
-}
-
-function processSegue(to, data) {
-	//console.log(to+":"+data)
-	components[to].process(data);
-}
-
-components[config.startup.component].process(config.startup.url);
+new Spiderman(config).start();
